d139996efd156585c5e50aca520c21ba705e68e8
Setota Solomon <setotasolomon@D-173-250-149-227.dhcp4.washington.edu>
2016-11-03 10:13:01 -0700

    did excecie 4

--- exercise-4/main.jsx
+++ exercise-4/main.jsx
@@ -50,16 +50,23 @@ var EmployeeSearch = React.createClass({
     getInitialState:function() {
         return({searchString:''});
     },
-    // Add a filter funciton
-
+    filter:function(e) {
+        console.log(e.target.value);
+        this.setState({searchString:e.target.value})
+    },
     render:function() {
-        var employees = this.props.data;
+        var employees = this.props.data,
+           searchString = this.state.searchString.trim().toLowerCase();
 
-        // Use this.state.searchString to filter down the `employees` array
-
+        if(searchString.length > 0){
+           // We are searching. Filter the results.
+           employees = employees.filter(function(employee){
+               return employee.name.toLowerCase().match( searchString );
+           });
+        }
         return(
             <div>
-                <input placeholder="Search employees"/>
+                <input onChange={this.filter} placeholder="Search employees"/>
                 <EmployeeTable data={employees}/>
             </div>
         )
